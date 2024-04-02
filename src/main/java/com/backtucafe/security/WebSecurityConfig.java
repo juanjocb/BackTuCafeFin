40ec16c8bdec79256b522758d231e882c1b61300
Juan Jose <juanjo2003gg@gmail.com>
2024-04-01 21:35:18 -0500

    Cambio cors

--- src/main/java/com/backtucafe/security/WebSecurityConfig.java
+++ src/main/java/com/backtucafe/security/WebSecurityConfig.java
@@ -34,32 +34,41 @@ public class WebSecurityConfig {
 //               .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/lugares", "/acerca", "/opiniones", "/reserva",
-                                "/tuCafe/v1/business/register", "/tuCafe/v1/client/register", "/tuCafe/v1/client/login",
-                                "/tuCafe/v1/business/login", "/tuCafe/v1/client/{id_client}", "/tuCafe/v1/client/listBusiness",
-                                "/tuCafe/v1/business/{id_business}", "/tuCafe/v1/reservation/creation_reservation",
-                                "/tuCafe/v1/admin/login", "/tuCafe/v1/admin/getAdmin", "/tuCafe/v1/admin/changeStatus/{idBusiness}",
-                                "/tuCafe/v1/business/getBusinessNotActive", "/tuCafe/v1/business/getBusinessActive", "/tuCafe/v1/client/listBusiness", "/tuCafe/v1/view/newView",
-                                "/tuCafe/v1/image/upload/{idBusiness}", "/tuCafe/v1/business/{idBusiness}", "/tuCafe/v1/image/{idBusiness}", "/tuCafe/v1/view/views",
-                                "/tuCafe/v1/reservation/reservaBusiness/{businessId}", "/tuCafe/v1/reservation/reservaClient/{clientId}",
-                                "tuCafe/v1/business/changeStatusReservation/{reservationId}",
-                                "tuCafe/v1/business/{idBusiness}/changePassword", "/tuCafe/v1/client/put/{id_client}", "/tuCafe/v1/image/upload/{idBusiness}", "/tuCafe/v1/image/upload/1").permitAll()
+                        .requestMatchers(
+                                "/",
+                                "/lugares",
+                                "/acerca",
+                                "/opiniones",
+                                "/reserva",
+                                "/tuCafe/v1/business/register",
+                                "/tuCafe/v1/client/register",
+                                "/tuCafe/v1/client/login",
+                                "/tuCafe/v1/business/login",
+                                "/tuCafe/v1/client/{id_client}",
+                                "/tuCafe/v1/client/listBusiness",
+                                "/tuCafe/v1/business/{id_business}",
+                                "/tuCafe/v1/reservation/creation_reservation",
+                                "/tuCafe/v1/admin/login",
+                                "/tuCafe/v1/admin/getAdmin",
+                                "/tuCafe/v1/admin/changeStatus/{idBusiness}",
+                                "/tuCafe/v1/business/getBusinessNotActive",
+                                "/tuCafe/v1/business/getBusinessActive",
+                                "/tuCafe/v1/view/newView",
+                                "/tuCafe/v1/image/upload/{idBusiness}",
+                                "/tuCafe/v1/business/{idBusiness}",
+                                "/tuCafe/v1/image/{idBusiness}",
+                                "/tuCafe/v1/view/views",
+                                "/tuCafe/v1/reservation/reservaBusiness/{businessId}",
+                                "/tuCafe/v1/reservation/reservaClient/{clientId}",
+                                "/tuCafe/v1/business/changeStatusReservation/{reservationId}",
+                                "/tuCafe/v1/business/{idBusiness}/changePassword",
+                                "/tuCafe/v1/client/put/{id_client}",
+                                "/tuCafe/v1/image/upload/{idBusiness}",
+                                "/tuCafe/v1/image/upload/1").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(sessionM -> sessionM.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.addAllowedOriginPattern("*"); // Permitir cualquier origen
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 
     @Bean
